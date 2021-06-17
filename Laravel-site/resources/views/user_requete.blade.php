4fae807585f234be96a7ff895cf35b371bf20e0f
dayana-18 <65567834+dayana-18@users.noreply.github.com>
2021-06-18 01:35:59 +0200

    Update user_requete.blade.php

--- Laravel-site/resources/views/user_requete.blade.php
+++ Laravel-site/resources/views/user_requete.blade.php
@@ -9,20 +9,20 @@ profil
 <div class="row"  style="margin-left: 0.5rem;">
 @forelse($user->requetes as $requete)
   <div class="col-sm-4">
-    <div class="card h-100">
-      <div class="card-header" style="max-width: 40rem;">{{ $requete->adresse }}</div>
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">
-
+    <div class="card h-100"  > <!--style="max-width: 21rem;" -->
+      <div class="card-header" style="font-weight:bold">{{ $requete->adresse }}</div>
+      <div class="card-body" style="text-align: center ">
+        <!--<h5 class="card-title" ></h5>-->
+        <p class="card-text" >
           {{ $requete-> type_bien }}<br>
           {{ $requete-> age_bien }}<br>
-          {{ $requete-> nombre_pieces }} pièces<br>
-          Avec un prix allant de {{ $requete-> prix_min }} à {{ $requete-> prix_max }}€<br>
-
+          {{ $requete-> nombre_pieces }} pièce(s)<br>
+          Avec un prix allant de {{ $requete-> prix_min }} à {{ $requete-> prix_max }}€
         </p>
-        <a href="#" class="btn btn-primary" style="margin-right:15px;">Voir sur la carte</a>
-        <a href="{{ route('requete.delete', [ 'id' => $user->id ,'reqid' => $requete->id]) }}" class="btn btn-danger">Supprimer</a>
+      </div>
+      <div class="card-footer" style="text-align: center;">
+        <a href="#" class="btn btn-outline-primary" style="margin-right:15px;">Voir sur la carte</a>
+        <a href="{{ route('requete.delete', [ 'id' => $user->id ,'reqid' => $requete->id]) }}" class="btn btn-outline-danger">Supprimer</a>
       </div>
     </div>
     <br>
