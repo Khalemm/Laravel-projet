44779dd4ceec7619215ce5e79d82afc83c96802c
Luca MARTINOLLI <martinolli.luca@gmail.com>
2021-06-22 18:08:51 +0200

    ajout de la page d'accueil + ptit footer

--- Laravel-site/resources/views/accueil.blade.php
+++ Laravel-site/resources/views/accueil.blade.php
@@ -8,7 +8,21 @@ Accueil
 
 @section('contenu')
 <div class="accueil-bloc">
+    <div class="presentation-bloc">
+        <div class="interieur-presentation-bloc">
+            <div class="titre-presentation">
+                <h2>Estimez le prix des biens !</h2>
+            </div>
+            <div class="contenu-presentation">
+                <p> Ici vous pourrez estimer le prix des biens près de chez vous </p>
+                <p> texte de remplissage </p>
+                <p> je sais pas quoi mettre encore ici ;_;</p>
+                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam, voluptate ipsum corporis suscipit voluptates repellat obcaecati molestias fuga praesentium dolore quos, aperiam perspiciatis voluptatem magnam nam minima placeat. Reprehenderit, eum?</p>
+            </div>
+        </div>
+    </div>
 </div>
+@include('partials.footer')
 @endsection
 
 @section('scripts-pied')
---
+++ Laravel-site/resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © 2021 - {{ now()->year }} Data4Sports. Tous droits réservés.</p>
+    </div>
+</div>
