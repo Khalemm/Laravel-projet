cfb267d91edd363034953c1fe4db7f267e52a351
dayana-18 <65567834+dayana-18@users.noreply.github.com>
2021-07-05 14:25:49 +0200

    Update adminGestionUser.blade.php

--- Laravel-site/resources/views/adminGestionUser.blade.php
+++ Laravel-site/resources/views/adminGestionUser.blade.php
@@ -62,7 +62,6 @@ class="btn btn-danger">Supprimer les utilisateurs non vérifiés</a></li></ul>
                         <a href="{{ route('user.abonnement', [ 'id' => $user->id] ) }}" class="btn btn-warning">Abonnement</a>
                     @endif
                     
-                    <a href="{{ route('user.admin', [ 'id' => $user->id] ) }}" class="btn btn-primary">Rendre Admin</a>
                     <a href="{{ route('user.delete', [ 'id' => $user->id] ) }}" onclick="return confirm('Confirmer la suppression de l`utilisateur')" class="btn btn-danger">Supprimer</a>
                     
                 </div>
